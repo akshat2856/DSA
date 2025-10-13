ed37410bbe919276bd5f5552a13111caa1d75f87
Akshat Srivastava <srivastavakshat2004@gmail.com>
2025-10-13 22:08:34 +0530

    Time: 2 ms (93.48%) | Memory: 44.5 MB (96.96%) - LeetSync

---
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> removeAnagrams(String[] words) {
+      ArrayList<String> result = new ArrayList<>();
+        String prevSorted = "";
+
+        for (String word : words) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+
+            if (!sortedWord.equals(prevSorted)) {
+                result.add(word);     
+                prevSorted = sortedWord;
+            }
+        }
+
+        return result;
+
+    }
+}
