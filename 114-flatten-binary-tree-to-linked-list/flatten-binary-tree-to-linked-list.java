39a70793959c2323042a942681746f2befbacbe0
Akshat Srivastava <srivastavakshat2004@gmail.com>
2025-11-22 15:13:55 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (32.41%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -13,27 +13,48 @@
  *     }
  * }
  */
-class Solution {
-    public void flatten(TreeNode root) {
-        if (root == null) return;
+
+ //brute force
+// class Solution {
+//     public void flatten(TreeNode root) {
+//         if (root == null) return;
 
         
-        List<Integer> li = new ArrayList<>();
-        solve(root, li);
+//         List<Integer> li = new ArrayList<>();
+//         solve(root, li);
 
        
-        TreeNode curr = root;
-        curr.left = null;
-        for (int i = 1; i < li.size(); i++) {
-            curr.right = new TreeNode(li.get(i));
-            curr = curr.right;
-        }
-    }
+//         TreeNode curr = root;
+//         curr.left = null;
+//         for (int i = 1; i < li.size(); i++) {
+//             curr.right = new TreeNode(li.get(i));
+//             curr = curr.right;
+//         }
+//     }
 
-    public static void solve(TreeNode root, List<Integer> li) {
-        if (root == null) return;
-        li.add(root.val);
-        solve(root.left, li);
-        solve(root.right, li);
+//     public static void solve(TreeNode root, List<Integer> li) {
+//         if (root == null) return;
+//         li.add(root.val);
+//         solve(root.left, li);
+//         solve(root.right, li);
+//     }
+// }
+
+//optimal with O(1) sapce
+class Solution {
+    public void flatten(TreeNode root) {
+        TreeNode cur = root;
+        while (cur != null) {
+            if (cur.left != null) {
+                TreeNode prev = cur.left;
+                while (prev.right != null) {
+                    prev = prev.right;
+                }
+                prev.right = cur.right;
+                cur.right = cur.left;
+                cur.left = null;
+            }
+            cur = cur.right;
+        }
     }
 }
