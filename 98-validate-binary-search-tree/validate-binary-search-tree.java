fee318953cd25b3633fffc59a98afff18aec1ede
Akshat Srivastava <srivastavakshat2004@gmail.com>
2025-11-24 00:34:55 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (16.50%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -13,21 +13,37 @@
  *     }
  * }
  */
+// class Solution {
+//     public boolean isValidBST(TreeNode root) {
+//        List<Integer> li = new ArrayList<>();
+//         solve(root, li);
+//         for(int i =1;i<li.size();i++){
+//             if(li.get(i)<=li.get(i-1))return false;
+//         }
+//         return true;
+//     }
+
+//     public void solve(TreeNode root, List<Integer> li) {
+//         if (root == null) return;
+        
+//         solve(root.left, li);   
+//         li.add(root.val);                
+//         solve(root.right, li);       
+//     }
+// }
+
 class Solution {
     public boolean isValidBST(TreeNode root) {
-       List<Integer> li = new ArrayList<>();
-        solve(root, li);
-        for(int i =1;i<li.size();i++){
-            if(li.get(i)<=li.get(i-1))return false;
-        }
-        return true;
+        return findIfBST (root,Long.MAX_VALUE,Long.MIN_VALUE);
     }
 
-    public void solve(TreeNode root, List<Integer> li) {
-        if (root == null) return;
-        
-        solve(root.left, li);   
-        li.add(root.val);                
-        solve(root.right, li);       
+    public boolean findIfBST (TreeNode root,long max,long min) {
+        if(root == null)
+        return true;
+
+        if(root.val >= max || root.val <=min)
+        return false;
+
+        return findIfBST(root.left,root.val,min) && findIfBST (root.right,max,root.val) ;
     }
 }
