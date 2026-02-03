d0be85d906baaa3089675b3243d93e967658e561
Akshat Srivastava <srivastavakshat2004@gmail.com>
2026-02-03 18:54:53 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (13.98%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,18 +1,29 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        int[] t = new int[n];
-        for(int i=0;i<n ; i++){
-            t[i]=-1;
+        // int[] t = new int[n];
+        // for(int i=0;i<n ; i++){
+        //     t[i]=-1;
+        // }
+        // return f(n-1,nums,t);
+        int prev = nums[0];
+        int prev2 = 0;
+        for(int i=1;i<n;i++){
+            int take = nums[i];
+            if(i>1)take+=prev2;
+            int nontake = 0 + prev;
+            int curi = Math.max(take,nontake);
+            prev2 = prev;
+            prev = curi;
         }
-        return f(n-1,nums,t);
+        return prev;
     }
-    public int f(int ind,int[] nums,int[] t){
-      if(ind==0)return nums[ind];
-      if(ind<0)return 0;
-      if(t[ind]!=-1)return t[ind];
-      int pick = nums[ind] + f(ind-2,nums,t);
-      int non_pick = 0 + f(ind-1,nums,t);
-      return  t[ind] = Math.max(pick,non_pick);
-    }
+    // public int f(int ind,int[] nums,int[] t){
+    //   if(ind==0)return nums[ind];
+    //   if(ind<0)return 0;
+    //   if(t[ind]!=-1)return t[ind];
+    //   int pick = nums[ind] + f(ind-2,nums,t);
+    //   int non_pick = 0 + f(ind-1,nums,t);
+    //   return  t[ind] = Math.max(pick,non_pick);
+    // }
 }
