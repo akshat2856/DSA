dbf23624108cba20f9865e373c0bbd2ee96a1988
Akshat Srivastava <srivastavakshat2004@gmail.com>
2026-02-19 23:51:57 +0530

    Time: 109 ms (20.38%) | Memory: 77.6 MB (75.55%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -4,23 +4,21 @@ class Solution {
         int[] left = new int[n];
         int[] right = new int[n];
         Stack<Integer> st = new Stack<>();
-        Stack<Integer> pq = new Stack<>();
-        int max = Integer.MIN_VALUE;
-        for(int i=0;i<n;i++){
-            while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && heights[st.peek()] >= heights[i])
                 st.pop();
-            }
-            if(st.isEmpty())left[i] = -1;
-            else left[i] = st.peek();
+
+            left[i] = st.isEmpty() ? -1 : st.peek();
             st.push(i);
         }
-         for(int i=n-1;i>=0;i--){
-            while(!pq.isEmpty() && heights[pq.peek()]>=heights[i]){
-                pq.pop();
-            }
-            if(pq.isEmpty())right[i] = n;
-            else right[i] = pq.peek();
-            pq.push(i);
+        st.clear(); 
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && heights[st.peek()] >= heights[i])
+                st.pop();
+
+            right[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
         }
          for(int i=0;i<n;i++){
             max = Math.max(max,((right[i]-left[i]-1)*heights[i]));
