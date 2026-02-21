12606cd00c8fbdf92269a7bd3e8e21386bad5afc
Akshat Srivastava <srivastavakshat2004@gmail.com>
2026-02-21 15:21:07 +0530

    Time: 6 ms (90.06%) | Memory: 45.5 MB (31.06%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,20 +1,18 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-    Stack<Integer> val = new Stack<>();
-    for (int i = 0; i < tokens.length; i++) {
-    String token = tokens[i];
-    if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
-    int v2 = val.pop();
-    int v1 = val.pop();
-    if (token.equals("+")) val.push(v1 + v2);
-    else if (token.equals("-")) val.push(v1 - v2);
-    else if (token.equals("*")) val.push(v1 * v2);
-    else if (token.equals("/")) val.push(v1 / v2);
-     }
-     else {   
-         val.push(Integer.parseInt(token));
-            }
+        int n = tokens.length;
+       Stack<Integer> val = new Stack<>();
+       for(int i=0;i<n;i++){
+        if(tokens[i].equals("+")||tokens[i].equals("-")||tokens[i].equals("*") ||tokens[i].equals("/")){
+        int val2 = val.pop();
+        int val1 = val.pop();
+        if(tokens[i].equals("+"))val.push(val1+val2);
+        else if(tokens[i].equals("-"))val.push(val1-val2);
+        else if(tokens[i].equals("*"))val.push(val1*val2);
+        else if(tokens[i].equals("/"))val.push(val1/val2);
         }
-        return val.peek();
+        else val.push(Integer.parseInt(tokens[i]));
+       } 
+       return val.peek();
     }
 }
