af9665902890a4db82e77fa8117422a3dcbba0c7
Akshat Srivastava <srivastavakshat2004@gmail.com>
2026-02-23 16:24:44 +0530

    Time: 2 ms (96.07%) | Memory: 48.4 MB (60.82%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,27 +1,14 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-         return search(numbers, 0, target);
-    }
-      static int[] search(int[] arr, int low, int target) {
-        if (low >= arr.length - 1)
-            return new int[]{-1, -1};
-        int complement = target - arr[low];
-        int index = binarySearch(arr, low + 1, arr.length - 1, complement);
-        if (index != -1) {
-            return new int[]{low + 1, index + 1};
+        int n = numbers.length;
+        int left = 0;
+        int right = n-1;
+        while(left<right){
+            int sum = numbers[left] + numbers[right];
+            if(sum==target)return new int[] {left+1,right+1};
+            if(sum>target)right--;
+            if(sum<target)left++;
         }
-        return search(arr, low + 1, target);
-    }
-     static int binarySearch(int[] arr, int low, int high, int target) {
-        if (low > high)
-            return -1;
-        int mid = low + (high - low) / 2;
-        if (arr[mid] == target)
-            return mid;
-        else if (target < arr[mid])
-            return binarySearch(arr, low, mid - 1, target);
-
-        else
-            return binarySearch(arr, mid + 1, high, target);
+        return new int[] {-1,-1};
     }
 }
