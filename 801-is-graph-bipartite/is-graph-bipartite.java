ed0f84c186919d8ab282739903287587b2186bac
Akshat Srivastava <srivastavakshat2004@gmail.com>
2026-03-02 19:41:07 +0530

    Time: 1 ms (63.69%) | Memory: 47.1 MB (90.34%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -25,6 +25,7 @@ class Solution {
         int[] vis = new int[n];
         Arrays.fill(vis,-1);
         for(int i=0;i<n;i++){
+            if(ans==false)return false;
             if(vis[i]==-1)bfs(i,graph,vis);
         }
         return ans;
