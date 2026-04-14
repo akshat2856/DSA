b5aa68504f24c74e1616103039e904d384f27b76
Akshat Srivastava <srivastavakshat2004@gmail.com>
2026-04-15 00:30:19 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (33.69%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,39 +1,21 @@
 class Solution {
-   public int findCircleNum(int[][] adj) {
-      int n = adj.length;
-      int count = 0;
-      boolean[] vis = new boolean[n];
-      for(int i=0;i<n;i++){
-          if(!vis[i]){
-             // bfs(i,vis,adj);
-             dfs(i,vis,adj);
-              count++;
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        boolean[] vis = new boolean[n];
+        int count = 0;
+        for(int i=0;i<n;i++){
+          if(!vis[i]) {
+             dfs(i,vis,n,isConnected);
+            count++;
           }
-      }
-      return count;
-    }
-      private void dfs(int i, boolean[] vis, int[][] adj) {
-        int n = adj.length;
-        vis[i] = true;
-       for(int j=0;j<n;j++){
-        if(adj[i][j]==1 && vis[j]==false){
-            dfs(j,vis,adj);
         }
-       }
+        return count;
     }
-
-    private void bfs(int i, boolean[] vis, int[][] adj) {
-        int n = adj.length;
-        vis[i] = true;
-        Queue<Integer> q = new LinkedList<>();
-        q.add(i);
-        while(q.size()>0){
-            int front = q.remove();
-            for(int j=0;j<n;j++){
-                if(adj[front][j]==1 && vis[j]==false){
-                    q.add(j);
-                    vis[j] = true;
-                }
+    public void dfs(int node,boolean[] vis,int n,int[][] isConnected){
+        vis[node] = true;
+        for(int i=0;i<n;i++){
+            if(isConnected[node][i]==1 && !vis[i]){
+                dfs(i,vis,n,isConnected);
             }
         }
     }
