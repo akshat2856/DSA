b89a9b71beea8b3938c3bd3555ef106ae5b084ca
Akshat Srivastava <srivastavakshat2004@gmail.com>
2026-05-26 01:28:07 +0530

    Time: 33 ms (20.32%) | Memory: 48.7 MB (14.69%) - LeetSync

--- 575-distribute-candies/distribute-candies.java
+++ 575-distribute-candies/distribute-candies.java
@@ -5,7 +5,6 @@ class Solution {
         for(int i=0;i<n;i++){
             set.add(candyType[i]);
         }
-        if((n/2)<=set.size()) return n/2;
-        return set.size();
+        return Math.min(n/2,set.size());
     }
 }
