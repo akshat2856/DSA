fc8496c100fe9cc7f4c64b6eec61a5a1dba7b514
Akshat Srivastava <srivastavakshat2004@gmail.com>
2026-05-30 17:12:53 +0530

    Time: 24 ms (28.71%) | Memory: 47.6 MB (97.90%) - LeetSync

--- 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.java
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.java
@@ -7,7 +7,7 @@ class Solution {
             if(Character.isLowerCase(ch)){
             //    if(big[ch-'a'] == -1)big[ch-'a'] = 0;
                 if(big[ch-'a'] == 1)small[ch-'a'] = 0;
-                else if(big[ch-'a'] != 1 && big[ch-'a'] != -1) small[ch-'a'] = 1;
+                else if( big[ch-'a'] != -1) small[ch-'a'] = 1;
             }
             else{
                 if(small[ch-'A']==1){
