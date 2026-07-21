01bfcc06b22bb3e2e13c5c22c66b49e188f6a3cc
Akshat Srivastava <srivastavakshat2004@gmail.com>
2026-07-21 23:51:01 +0530

    Time: 583 ms (24.88%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -1,6 +1,6 @@
-# Write your MySQL query statement below
-select p.project_id,ROUND(sum(e.experience_years)/count(p.project_id),2) as average_years
-from Project as p 
+select p.project_id,ROUND(SUM(e.experience_years)/count(p.project_id),2) as average_years
+from Project as p
 left join Employee as e
-on p.employee_id = e.employee_id
-group by p.project_id;
+on
+p.employee_id=e.employee_id
+group by p.project_id
