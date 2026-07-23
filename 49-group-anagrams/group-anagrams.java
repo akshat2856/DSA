2d22e9c6e77768214aa52a4db83310646c24083f
Akshat Srivastava <srivastavakshat2004@gmail.com>
2026-07-24 00:00:50 +0530

    Time: 7 ms (73.75%) | Memory: 49.9 MB (26.36%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,64 +1,21 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        // int n = strs.length;
-        // boolean[] visited = new boolean[n];
-        // List<List<String>> result = new ArrayList<>();
-        // for(int i=0;i<n;i++){
-        //     if(visited[i])continue;
-        //     List<String> group = new ArrayList<>();
-        //     group.add(strs[i]);
-        //     visited[i] = true;
-        //     for(int j=i+1;j<n;j++){
-        //         if(!visited[j] && isAnagrams(strs[i],strs[j])){
-        //             group.add(strs[j]);
-        //             visited[j] = true;
-        //         }
-        //     }
-        //     result.add(group);
-        // }
-        // return result;
-        if(strs==null||strs.length==0)return new ArrayList<>();
-        Map<String,List<String>> freqMap = new HashMap<>();
-        for(String str:strs){
-            String freqString = getfreqString(str);
-            if(freqMap.containsKey(freqString))  freqMap.get(freqString).add(str);
-            else{
-                List<String> strList = new ArrayList<>();
-                strList.add(str);
-                 freqMap.put(freqString,strList);
+        int n = strs.length;
+        HashMap<String,List<String>> map = new HashMap<>();
+        List<List<String>> list = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            String s = strs[i];
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String p = new String(arr);
+            if(!map.containsKey(p)){
+                map.put(p,new ArrayList<>());
             }
+                map.get(p).add(s);
         }
-        return new ArrayList<>(freqMap.values());
-    }
-    // boolean isAnagrams(String s,String t){
-    //  s = s.toLowerCase();
-    //  t = t.toLowerCase();
-    //  s = s.replace(" ","");
-    //  t = t.replace(" ","");
-    //  int[] counts = new int[26];
-    //  for(int i=0;i<s.length();i++){
-    //     counts[s.charAt(i)-'a']++;
-    //  }
-    //  for(int i=0;i<t.length();i++){
-    //     counts[t.charAt(i)-'a']--;
-    //  }
-    //  for(int count:counts){
-    //     if(count!=0)return false;
-    //  }
-    //  return true;
-    // }
-    private String getfreqString(String str){
-        int[] freq = new int[26];
-        for(char c:str.toCharArray()){
-            freq[c-'a']++;
+        for(Map.Entry<String,List<String>> entry : map.entrySet()){
+            list.add(entry.getValue());
         }
-        StringBuilder freqString = new StringBuilder("");
-        char c = 'a';
-        for(int i:freq){
-            freqString.append(c);
-            freqString.append(i);
-            c++;
-        }
-        return freqString.toString();
+        return list;
     }
 }
