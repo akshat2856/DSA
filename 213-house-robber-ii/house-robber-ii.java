783b190a1e33c0a680a7fbe196742217d6ef9c2b
Akshat Srivastava <srivastavakshat2004@gmail.com>
2026-07-24 21:27:21 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (99.27%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,29 +1,31 @@
 class Solution {
     public int rob(int[] nums) {
-          int n = nums.length;
-          int[] x = new int[n-1];
-          int[] y = new int[n-1];
-          for(int i=0;i<n-1;i++){
-            x[i] = nums[i+1];
-          }
-          for(int i=0;i<n-1;i++){
-            y[i] = nums[i];
-          }
-          if(n==1)return nums[0];
-          return Math.max(solve(x),solve(y));
-    }
-    public int solve(int[] nums){
         int n = nums.length;
-        int prev = nums[0];
-        int prev2 = 0;
-        for(int i=1;i<n;i++){
-            int take = nums[i];
-            if(i>1)take+=prev2;
-            int nontake = 0 + prev;
-            int curi = Math.max(take,nontake);
-            prev2 = prev;
-            prev = curi;
-        }
-        return prev;
+        if(n==0)return 0;
+        if(n==1)return nums[0];
+        int[] dp = new int[n];
+        Arrays.fill(dp,-1);
+        int[] bp = new int[n];
+        Arrays.fill(bp,-1);
+        int x = solve(nums,n-2,dp);
+        int y = solve1(nums,n-1,bp);
+        return Math.max(x,y);
+    }
+    public int solve(int[] nums,int index,int[] dp){
+        //if(index==0)return nums[index];
+        if(index<0)return 0;
+        if(dp[index]!=-1)return dp[index];
+        int pick = nums[index] + solve(nums,index-2,dp);
+        int notpick = 0 + solve(nums,index-1,dp);
+        return dp[index] = Math.max(pick,notpick);
+    }
+    public int solve1(int[] nums,int index,int[] bp){
+        //if(index==0)return nums[index];
+        if(index<1)return 0;
+       // if (index == 1) return nums[1];
+        if(bp[index]!=-1)return bp[index];
+        int pick = nums[index] + solve1(nums,index-2,bp);
+        int notpick = 0 + solve1(nums,index-1,bp);
+        return bp[index] = Math.max(pick,notpick);
     }
 }
