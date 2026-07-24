f9e9d815d8d163d857c99b7a2e3e02e986c09510
Akshat Srivastava <srivastavakshat2004@gmail.com>
2026-07-24 20:41:49 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (56.92%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,29 +1,16 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        // int[] t = new int[n];
-        // for(int i=0;i<n ; i++){
-        //     t[i]=-1;
-        // }
-        // return f(n-1,nums,t);
-        int prev = nums[0];
-        int prev2 = 0;
-        for(int i=1;i<n;i++){
-            int take = nums[i];
-            if(i>1)take+=prev2;
-            int nontake = 0 + prev;
-            int curi = Math.max(take,nontake);
-            prev2 = prev;
-            prev = curi;
-        }
-        return prev;
+        int[] dp = new int[n];
+        Arrays.fill(dp,-1);
+        return solve(nums,n-1,dp);
     }
-    // public int f(int ind,int[] nums,int[] t){
-    //   if(ind==0)return nums[ind];
-    //   if(ind<0)return 0;
-    //   if(t[ind]!=-1)return t[ind];
-    //   int pick = nums[ind] + f(ind-2,nums,t);
-    //   int non_pick = 0 + f(ind-1,nums,t);
-    //   return  t[ind] = Math.max(pick,non_pick);
-    // }
+    public int solve(int[] nums,int index,int[] dp){
+        if(index==0)return nums[index];
+        if(index<0)return 0;
+        if(dp[index]!=-1)return dp[index];
+        int pick = nums[index] + solve(nums,index-2,dp);
+        int notpick = 0 + solve(nums,index-1,dp);
+        return dp[index] = Math.max(pick,notpick);
+    }
 }
