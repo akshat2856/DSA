6f7ad1ec81a1c664e6f80c23c3940f61e15690ac
Akshat Srivastava <srivastavakshat2004@gmail.com>
2026-07-30 17:47:25 +0530

    Time: 1 ms (87.11%) | Memory: 45.4 MB (62.90%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -3,10 +3,7 @@ class Solution {
         List<List<Integer>> list = new ArrayList<>();
         boolean[] visited =new boolean[nums.length];
         solve(nums,list,new ArrayList<>(),visited);
-        List<List<Integer>> uniqueLists = list.stream()
-    .distinct()
-    .collect(Collectors.toList());
-    return uniqueLists;
+        return list;
     }
     public void solve(int[] nums,List<List<Integer>> list,List<Integer> ans,boolean[] visited){
         if(ans.size()==nums.length){
