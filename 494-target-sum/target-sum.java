c0a050b8c33c9b72b6dc189dd7999461dc68717b
Akshat Srivastava <srivastavakshat2004@gmail.com>
2026-08-02 00:37:13 +0530

    Time: 11 ms (50.80%) | Memory: 44.8 MB (50.46%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,25 +1,25 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        int n= nums.length;
-        int totalsum =0;
-        for (int ele : nums) {
-            totalsum += ele;
+        int n = nums.length;
+        int sum =0;
+        for(int num : nums){
+            sum+=num;
         }
-        if (Math.abs(target) > totalsum) return 0;
-        if ((target + totalsum) % 2 != 0) return 0;
-        int sum = (target + totalsum) / 2;
-        int result = subsetsum(nums,sum,n);
-         return result;
+        if(Math.abs(target) > sum)return 0;
+        if((target+sum)%2!=0)return 0;
+        int newsum = (sum+target)/2;
+        int ans = sub(nums,n,newsum);
+        return ans;
     }
-    public static int subsetsum(int[] nums,int sum,int n){
-        int[][] t = new int[n+1][sum+1];
-       t[0][0] =1;
+    public int sub(int[] nums,int n,int newsum){
+        int[][] dp = new int[n+1][newsum+1];
+        dp[0][0] = 1;
         for(int i=1;i<n+1;i++){
-            for(int j=0 ; j<sum+1 ; j++){
-                if(nums[i-1]<=j)t[i][j] = t[i-1][j-nums[i-1]] + t[i-1][j];
-                else t[i][j] =   t[i-1][j];
+            for(int j=0;j<newsum+1;j++){
+                if(nums[i-1]<=j)dp[i][j]=dp[i-1][j-nums[i-1]] + dp[i-1][j];
+                else dp[i][j] = dp[i-1][j];
             }
         }
-        return t[n][sum];
+        return dp[n][newsum];
     }
 }
