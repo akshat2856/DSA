ef1c92f310caf279c02ac6345aac8f9c91d1a6d6
Akshat Srivastava <srivastavakshat2004@gmail.com>
2026-08-01 12:12:07 +0530

    Time: 67 ms (35.82%) | Memory: 47.3 MB (61.09%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,33 +1,27 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         int n = nums.length;
-        int sum =0;
-       for(int ele : nums){
-sum+=ele;
-       } 
-       if(sum%2!=0)return false;
-       else if(sum%2==0){
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+        if(sum%2!=0)return false;
         return subsetsum(nums,sum/2,n);
-       }
-       return false;
     }
-    public boolean  subsetsum(int[] arr,int sum,int n){
-        boolean[][] t = new boolean[n+1][sum+1];
+    public boolean subsetsum(int[] nums,int sum,int n){
+        boolean[][] dp = new boolean[n+1][sum+1];
         for(int i=0;i<n+1;i++){
             for(int j=0;j<sum+1;j++){
-                if(i==0)t[i][j] = false;
-                if(j==0)t[i][j] = true;
+                if(i==0)dp[i][j] = false;
+                if(j==0)dp[i][j] = true;
             }
         }
         for(int i=1;i<n+1;i++){
             for(int j=1;j<sum+1;j++){
-                if(arr[i-1]<=j)
-                    t[i][j] = t[i-1][j-arr[i-1]] || t[i-1][j];
-                    else
-                        t[i][j] = t[i-1][j];
+                if(nums[i-1]<=j)dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                else dp[i][j] = dp[i-1][j];
+            }
         }
+        return dp[n][sum];
     }
-     return t[n][sum];
-
-}  
 }
