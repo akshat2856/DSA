53f840d5b7383ae474d71c2ab6dce58815b816c4
Akshat Srivastava <srivastavakshat2004@gmail.com>
2026-08-18 21:58:48 +0530

    Time: 7 ms (1.85%) | Memory: 43.9 MB (11.81%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -2,20 +2,17 @@ class Solution {
     public boolean isSubsequence(String s, String t) {
         int m = s.length();
         int n = t.length();
-        int result = L_cs(s,t);
-        if(result==m)return true;
-        else return false;
+        int ss = lcs(s,t,m,n);
+        return(ss==m);
     }
-    public int L_cs(String s,String p){
-        int m = s.length();
-        int n = p.length();
-        int[][] t = new int[m+1][n+1];
+    public int lcs(String s,String t,int m,int n){
+        int[][] dp = new int[m+1][n+1];
         for(int i=1;i<m+1;i++){
             for(int j=1;j<n+1;j++){
-                if(s.charAt(i-1)==p.charAt(j-1))t[i][j] = 1+t[i-1][j-1];
-                else t[i][j] = Math.max(t[i-1][j],t[i][j-1]);
+                if(s.charAt(i-1)==(t.charAt(j-1)))dp[i][j] = 1+dp[i-1][j-1];
+                else dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
             }
         }
-        return t[m][n];
+        return dp[m][n];
     }
 }
