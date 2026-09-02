fce4b9e9e380b39cbeabd1f46b06a5909881850c
Akshat Srivastava <srivastavakshat2004@gmail.com>
2026-09-02 18:04:24 +0530

    Time: 3 ms (17.89%) | Memory: 47.6 MB (76.42%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -11,7 +11,7 @@ class Solution {
             if(nums[i]!=0 && nums[j]==0){
                 i++;
             }
-              if(nums[i]!=0 && nums[j]!=0){
+            if(nums[i]!=0 && nums[j]!=0){
                 i++;
             }
             j++;
